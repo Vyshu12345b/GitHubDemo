6bdc0c3beb33f18c1a815ad5def3ea865b68ac14
Pallavi <banala.pallavi@capgemini.com>
2023-02-20 12:37:43 +0530

    first commit

---
+++ src/main/java/com/Pages/LoginPage.java
@@ -0,0 +1,11 @@
+package com.Pages;
+
+public class LoginPage {
+	public void  loginPage() {
+		System.out.println("This is loginPage");
+		
+	}
+}
+
+	
+
---
+++ src/test/java/com/Testcases/LoginTest.java
@@ -0,0 +1,14 @@
+package com.Testcases;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public void  loginTest() {
+			System.out.println("This is loginTest");
+			
+		}
+
+	}
+
+
